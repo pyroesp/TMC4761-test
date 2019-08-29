47f98696693637811840956963f86c121d9cd55d
pyroesp <nicolaslopezesp@gmail.com>
2019-08-29 21:39:23 +0200

    update main.c

--- main.c
+++ main.c
@@ -1,27 +1,38 @@
 #include "tmc/ic/TMC4671/TMC4671.h"
 
+SPISettings settingsA(something, something, something);
+
+setup(){
+    // Do SPI setup here
+    SPI.begin();
+
+    // Do motor setup here
+    tmc4671_setMotorType(motor, motorType);
+    tmc4671_setPolePairs(motor, polePairs);
+}
+
 uint8_t spi_transfer(uint8_t data, uint8_t lastTransfer){
     // Make your own spi transfer function
+    uint8_t received_data = 0;
 
-    return lastTransfer;
+    SPI.settings(settingsA); // apply SPI settings
+    digitalWrite(SS, LOW); // set SS low to enable data transfer to chip
+    received_data = SPI.transfer(data); // send data byte
+    // Assuming SPI.transfer send a byte and waits until it's done
+    if (lastTransfer) // if last byte in transfer packet
+        digitalWrite(SS,HIGH); // set SS high to disable data transfer
+
+    return received_data;
 }
 
 uint8_t tmc4671_readwriteByte(uint8_t motor, uint8_t data, uint8_t lastTransfer){
     // defined as external function in TMC4671.c
     // see also the evaluation board demo files IIRC
-    if (motor){
+    if (motor == DEFAULT_MOTOR){
         return spi_transfer(data, lastTransfer);
     }else{
         return 0;
     }
-}
-
-setup(){
-    // Do SPI setup here
-
-    // Do motor setup here
-    tmc4671_setMotorType(motor, motorType);
-    tmc4671_setPolePairs(motor, polePairs);
 }
 
 loop()

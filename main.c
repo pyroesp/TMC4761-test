e43967a16885e1ce5e028110bde60536c5491c57
pyroesp <nicolaslopezesp@gmail.com>
2019-08-29 19:28:32 +0200

    update main.c

--- main.c
+++ main.c
@@ -1,17 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-
 #include "tmc/ic/TMC4671/TMC4671.h"
 
 uint8_t spi_transfer(uint8_t data, uint8_t lastTransfer){
     // Make your own spi transfer function
+
     return lastTransfer;
 }
 
 uint8_t tmc4671_readwriteByte(uint8_t motor, uint8_t data, uint8_t lastTransfer){
     // defined as external function in TMC4671.c
-    // see also the evaluation board demo files
+    // see also the evaluation board demo files IIRC
     if (motor){
         return spi_transfer(data, lastTransfer);
     }else{
@@ -19,10 +16,15 @@ uint8_t tmc4671_readwriteByte(uint8_t motor, uint8_t data, uint8_t lastTransfer)
     }
 }
 
+setup(){
+    // Do SPI setup here
 
-int main()
+    // Do motor setup here
+    tmc4671_setMotorType(motor, motorType);
+    tmc4671_setPolePairs(motor, polePairs);
+}
+
+loop()
 {
-    // test function
-    tmc4671_setTargetTorque_raw(1, 0xAABB);
-    return 0;
+    // do loop stuff here
 }
